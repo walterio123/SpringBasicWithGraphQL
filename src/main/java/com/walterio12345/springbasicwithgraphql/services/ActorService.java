20e22226e009c812f303ec8dd5d107ce5eb4a9f3
Victor Schweizer <walterio12345@gmail.com>
2022-02-09 08:44:04 -0300

    mutation

--- src/main/java/com/walterio12345/springbasicwithgraphql/services/ActorService.java
+++ src/main/java/com/walterio12345/springbasicwithgraphql/services/ActorService.java
@@ -1,15 +1,17 @@
 package com.walterio12345.springbasicwithgraphql.services;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.walterio12345.springbasicwithgraphql.entity.Actor;
 import com.walterio12345.springbasicwithgraphql.repository.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-public class ActorService implements GraphQLQueryResolver {
+public class ActorService implements GraphQLQueryResolver, GraphQLMutationResolver {
     @Autowired
     private ActorRepository actorRepository;
 
@@ -32,4 +34,10 @@ public class ActorService implements GraphQLQueryResolver {
         return actorRepository.findAll().size();
     }
 
+    @Transactional
+    public Actor updateAddress( Integer id,String address){
+        Actor actor=actorRepository.findById(id).get();
+        actor.setAddress(address);
+        return actor;
+    }
 }
